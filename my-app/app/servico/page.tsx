62e5e4061667d6673a409ad886958de1c2bc7ec6
Carlos Campos <caduferrazcampos04@gmail.com>
2024-10-31 21:25:53 -0300

    Update page.tsx
    Update page.tsx

---
+++ my-app/app/servico/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import HeaderBasic from '../_components/header-basic';
+import MainSection from '../_components/MainSection';
+import Footer from '../_components/Footer';
+
+export default function Home() {
+  return (
+    <>
+      <HeaderBasic />
+      <MainSection />
+      <Footer />
+    </>
+  );
+}
